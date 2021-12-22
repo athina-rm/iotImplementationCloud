75a67665ac5de63659f90c71697546c1ac44b34a
Athina <athinamannaraprayil@gmail.com>
2021-12-22 08:56:31 +0100

    added apidata

--- webPage/script.js
+++ webPage/script.js
@@ -8,4 +8,11 @@ fetch("https://gethubandapidata.azurewebsites.net/api/HttpTrigger1")
                 overhead.innerHTML += myJson[0].OverheadTankLevel;
                 document.getElementById('rainWater').innerHTML += myJson[0].rainWaterTankLevel;
                 console.log (myJson);
-      }) ;                 
+      }) ; 
+fetch("http://api.weatherapi.com/v1/current.json?key=3cb4286283984cdcbdf61123211412&q=10.005034,76.692141")
+.then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {                
+                document.getElementById('apirain').innerHTML += myJson.current.precip_mm;                
+      }) ;  
